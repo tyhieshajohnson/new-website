444bdce0e72cb7607beef76d5656aec2d26f3f84
tyhieshajohnson <johnsontyhiesha@gmail.com>
2025-03-21 07:06:59 +0400

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
+import About from './components/About';
 
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Navbar />
       <Hero />
+      <About />
     </main>
   );
 };
---
+++ src/components/About.jsx
@@ -0,0 +1,21 @@
+import React, { useState, useRef, useEffect } from 'react';
+
+const About = () => {
+    return (
+        <div className="relative h-dvh w-screen overflow-x-hidden">
+            <div className="block yhFLh jWXgbn">
+                <h1 className="cMHqyL">CREDIT CARD MANAGEMENT. REIMAGINED.</h1>
+                <h1 className="AzLpJ">everything you need.
+                     <br/>nothing you don't.</h1>
+                     <br/>
+                <p className="eLnQK">welcome to a credit card experience designed to feel like second nature. like
+                    <br/>muscle memory. life's logistics demand enough of your time, effort, and
+                    <br/>attention—managing your credit cards shouldn't add to the list.</p>
+                    <br/>
+                <p className="eLnQK">you can put your admin duties to rest. for good.</p>
+            </div>
+        </div>
+        );
+    };
+
+export default About;
