d520a56c6d79986c399757c6d09765ab1f9b6b5e
tyhieshajohnson <johnsontyhiesha@gmail.com>
2025-03-23 19:59:44 +0400

    Initial commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,107 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>I am the footer, yay!</div>
-        );
+        <div className="sc-1ryyl0a-1 Ghuwm">
+            <footer className="sc-1ryyl0a-0 duxYwl">
+                <div className="sc-1sojafr-0 hWCCxT">
+                    <div className="sc-1sojafr-1 iMXKCx">
+                        <img className="sc-1sojafr-6 bCFfMH lozad" src="https://web-images.credcdn.in/v2/_next/assets/images/landing/desktop/footer-logo.png" alt="cred logo" data-loaded="true" />
+                        <img className="sc-1sojafr-5 biUoey lozad" data-src="https://web-images.credcdn.in/v2/_next/assets/images/landing/security-final-2.png" alt="security logo" src="https://web-images.credcdn.in/v2/_next/assets/images/landing/security-final-2.png" data-loaded="true" />
+                        <div className="sc-1sojafr-8 dDPGkn">complete security. no asterisks.</div>
+                        <div className="sc-1sojafr-9 dshbKH">CRED encrypts all data and transactions to ensure a completely secure experience for our members.</div>
+                    </div>
+                    <div className="sc-1sojafr-12 dtEhqm">
+                        <div className="sc-1sojafr-15 biUVdo">
+                            <div className="sc-1sojafr-16 hlQyPr">now live</div>
+                            <img src="https://web-images.credcdn.in/v2/_next/assets/images/launch-banners/cards/now-live-wide.png?tr=q-95" className="sc-1sojafr-17 hIVSUW" alt="now live" />
+                        </div>
+                        <div className="sc-1sojafr-4 eeTlTA">
+                            <div className="sc-1sojafr-2 gmVWof">
+                                <div className="sc-1sojafr-3 iCNtsG">
+                                    <div className="sc-1sojafr-11 cSektw">UPGRADES</div>
+                                    <div className="sc-1sojafr-10 hRyylX">
+                                        <div><a href="https://cred.club/money" target="_blank" rel="noopener noreferrer" className="sc-1sojafr-19 hOXrWh">CRED money</a></div>
+                                        <div><a href="https://cred.club/mint" target="_blank" rel="noopener noreferrer" className="sc-1sojafr-19 hOXrWh">CRED mint</a></div>
+                                        <div><a href="https://cred.club/garage" target="_blank" rel="noopener noreferrer" className="sc-1sojafr-19 hOXrWh">CRED garage</a></div>
+                                        <div><a href="https://cred.club/escapes" target="_blank" rel="noopener noreferrer" className="sc-1sojafr-19 hOXrWh">CRED escapes</a></div>
+                                    </div>
+                                </div>
+                                <div className="sc-1sojafr-3 iCNtsG">
+                                    <div className="sc-1sojafr-11 cSektw">PAYMENTS</div>
+                                    <div className="sc-1sojafr-10 hRyylX">
+                                        <div><a href="https://cred.club/cred-pay" target="_blank" rel="noopener noreferrer" className="sc-1sojafr-19 hOXrWh">Scan &amp; Pay</a></div>
+                                        <div><a href="https://cred.club/tap" target="_blank" rel="noopener noreferrer" className="sc-1sojafr-19 hOXrWh">Tap to Pay</a></div>
+                                        <div><a href="https://cred.club/pay-via-upi" target="_blank" rel="noopener noreferrer" className="sc-1sojafr-19 hOXrWh">Pay anyone</a></div>
+                                        <div><a href="https://cred.club/upi-on-credit" target="_blank" rel="noopener noreferrer" className="sc-1sojafr-19 hOXrWh">RuPay cards on UPI</a></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="sc-1sojafr-2 gmVWof">
+                                <div className="sc-1sojafr-3 iCNtsG">
+                                    <div className="sc-1sojafr-11 cSektw">COMPANY</div>
+                                    <div className="sc-1sojafr-10 hRyylX">
+                                        <div><a href="https://cred.club/about" target="_blank" rel="noopener noreferrer" className="sc-1sojafr-19 hOXrWh">about CRED</a></div>
+                                        <div><a href="https://careers.cred.club/openings" target="_blank" rel="noopener noreferrer" className="sc-1sojafr-19 hOXrWh">careers</a></div>
+                                    </div>
+                                </div>
+                                <div className="sc-1sojafr-3 iCNtsG">
+                                    <div className="sc-1sojafr-11 cSektw">INSIDER PERKS</div>
+                                    <div className="sc-1sojafr-10 hRyylX">
+                                        <div><a href="https://cred.club/ipl" target="_blank" rel="noopener noreferrer" className="sc-1sojafr-19 hOXrWh">upgrade to UPI</a></div>
+                                    </div>
+                                </div>
+                                <div className="sc-1sojafr-3 iCNtsG">
+                                    <div className="sc-1sojafr-11 cSektw">DESIGN</div>
+                                    <div className="sc-1sojafr-10 hRyylX">
+                                        <div><a href="https://cred.club/neopop" target="_blank" rel="noopener noreferrer" className="sc-1sojafr-19 hOXrWh">NeoPOP</a></div>
+                                        <div><a href="https://cred.club/design" target="_blank" rel="noopener noreferrer" className="sc-1sojafr-19 hOXrWh">manifesto</a></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="sc-1sojafr-2 gmVWof">
+                                <div className="sc-1sojafr-3 iCNtsG">
+                                    <div className="sc-1sojafr-11 cSektw">RESOURCES</div>
+                                    <div className="sc-1sojafr-10 hRyylX">
+                                        <div><a href="https://cred.club/cred-pay/onboarding" target="_blank" rel="noopener noreferrer" className="sc-1sojafr-19 hOXrWh">partner with us</a></div>
+                                        <div><a href="https://cred.club/calculators" target="_blank" rel="noopener noreferrer" className="sc-1sojafr-19 hOXrWh">calculators</a></div>
+                                        <div><a href="https://cred.club/articles" target="_blank" rel="noopener noreferrer" className="sc-1sojafr-19 hOXrWh">articles</a></div>
+                                        <div><a href="https://engineering.cred.club" target="_blank" rel="noopener noreferrer" className="sc-1sojafr-19 hOXrWh">tech blog</a></div>
+                                        <div><a href="https://cred.club/check-your-credit-score/articles" target="_blank" rel="noopener noreferrer" className="sc-1sojafr-19 hOXrWh">credit score guide</a></div>
+                                        <div><a href="https://cred.club/credit-card-bill-payment-online/articles" target="_blank" rel="noopener noreferrer" className="sc-1sojafr-19 hOXrWh">credit card payment guide</a></div>
+                                        <div><a href="https://cred.club/customer-care" target="_blank" rel="noopener noreferrer" className="sc-1sojafr-19 hOXrWh">customer care</a></div>
+                                        <div><a href="https://zfrmz.in/10ASsFdqjAnCiBMpakMX" target="_blank" rel="noopener noreferrer" className="sc-1sojafr-19 hOXrWh">Dreampurse (HipBar) wallet refund form</a></div>
+                                    </div>
+                                </div>
+                                <div className="sc-1sojafr-3 iCNtsG">
+                                    <div className="sc-1sojafr-11 cSektw">POLICY</div>
+                                    <div className="sc-1sojafr-10 hRyylX">
+                                        <div><a href="https://cred.club/transaction-and-user-verification-policy" target="_blank" rel="noopener noreferrer" className="sc-1sojafr-19 hOXrWh">transaction &amp; user verification</a></div>
+                                        <div><a href="https://cred.club/privacy#compliance-with-google-oauth-api-scopes" target="_blank" rel="noopener noreferrer" className="sc-1sojafr-19 hOXrWh">Google API disclosure </a></div>
+                                        <div><a href="https://cred.club/upi-faqs" target="_blank" rel="noopener noreferrer" className="sc-1sojafr-19 hOXrWh">UPI FAQ &amp; grievances</a></div>
+                                        <div><a href="https://cred.club/return-policy" target="_blank" rel="noopener noreferrer" className="sc-1sojafr-19 hOXrWh">returns and refunds</a></div>
+                                        <div><a href="https://cred.club/security" target="_blank" rel="noopener noreferrer" className="sc-1sojafr-19 hOXrWh">security</a></div>
+                                        <div><a href="https://web-assets.cred.club/assets/docs/eop.pdf" target="_blank" rel="noopener noreferrer" className="sc-1sojafr-19 hOXrWh">equal opportunity policy</a></div>
+                                        <div><a href="https://cred.club/investor-relations" target="_blank" rel="noopener noreferrer" className="sc-1sojafr-19 hOXrWh">investor relations</a></div>
+                                        <div><a href="https://cred.club/legal/other-disclosures" target="_blank" rel="noopener noreferrer" className="sc-1sojafr-19 hOXrWh">other disclosures</a></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="sc-1sojafr-14 hXmhLb">
+                    <div className="sc-1sojafr-13 dTTmuO"></div>
+                    <div className="sc-1sojafr-18 edGIvt">
+                        <div className="sc-1sojafr-21 hASeTI">copyright © 2020-24 Dreamplug Technologies Pvt Ltd.</div>
+                        <div className="sc-1sojafr-20 eEBJKd">
+                            <a href="https://cred.club/privacy" target="_blank" rel="noopener noreferrer" className="sc-1sojafr-7 ItEkq">privacy policy</a>
+                            <a href="https://cred.club/terms" target="_blank" rel="noopener noreferrer" className="sc-1sojafr-7 ItEkq">terms and conditions</a>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    );
 };
 
 export default Footer;
