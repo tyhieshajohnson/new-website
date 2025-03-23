9e216798e9c0c8bab2b3f1955e02a3a255b1f78a
tyhieshajohnson <johnsontyhiesha@gmail.com>
2025-03-23 15:02:25 +0400

    Initial commit

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,8 +1,65 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React from 'react';
 
 const Carousel = () => {
     return (
-        <div> Carousel Cards </div>
+        <section>
+            <div className="gGwbWp">
+                <h1 className="fquCfu dYlzCk czXzVu">every touch is
+                    <br /> pure power.</h1>
+
+                <div className="hrudQH"></div>
+
+                <div className="hHnUWF">
+                    <div className="carousel-item">
+                        <video playsInline autoPlay muted loop src="/videos/desktop/swipe.mp4" type="video/mp4" />
+                        <div className="hdDaEA"></div>
+                        <div className="hpTdVv">
+                            <img src="/img/desktop/swipe-left.png" alt="swipe left" className="fBRsjM" />
+                        </div>
+                        <div className="lbxDUD">
+                            <p className="kkVKoh">
+                                SWIPE LEFT
+                                <span className="buBswu">on any card to manage payment history, card offers, and more.
+                                </span>
+                            </p>
+                        </div>
+                    </div>
+                    <div className="carousel-item">
+                        <video playsInline autoPlay muted loop className="edROEC" src="/videos/desktop/long-press.mp4"
+                        type="video/mp4" />
+
+                        <div className="sc-g6c3md-0 hdDaEA"></div>
+
+                        <div className="hpTdVv">
+                            <img src="public/img/desktop/tap-hold.png"
+                            alt="tap hold" className="fBRsjM" />
+                            <div className="lbxDUD">
+                                <p className="kkVKoh">PRESS AND HOLD
+                                    <span className="buBswu">
+                                        a card to view balances, usage limits, recent activity and other key details.
+                                    </span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="carousel-item">
+                        <video playsInline autoPlay muted loop className="edROEC" src="/videos/desktop/smart-nav.mp4"
+                        type="video/mp4" />
+
+                        <div className="hdDaEA"></div>
+
+                        <div className="hpTdVv">
+                            <img src="/img/desktop/smart-nav.png" alt="smart nav" className="fBRsjM" />
+                            <div className="lbxDUD">
+                                <p className="kkVKoh">SMART NAVIGATION
+                                    <span className="buBswu">makes switching between cards seamless</span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
